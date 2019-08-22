2627124319536c9871e9db542296825b46bdb9f0
jiangtao.liang <jiangtao.liang@17zuoye.com>
2019-08-22 20:10:03 +0800

    optimize code

---
+++ app/src/main/java/com/moon/beautygirlkotlin/my_favorite/component/MyItemTouchHelperCallBack.kt
@@ -0,0 +1,35 @@
+package com.moon.beautygirlkotlin.my_favorite.component
+
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
+
+/**
+ * Created by moon on 2017/9/8.
+ */
+
+class MyItemTouchHelperCallBack(internal var mItemMoveListener: ItemMoveListener) : ItemTouchHelper.Callback() {
+
+
+    override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
+        val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+
+        // 如果你不想左右滑动，可以将 swipeFlags = 0
+        val swipeFlags = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+
+        //最终的动作标识（flags）必须要用makeMovementFlags()方法生成
+        return makeMovementFlags(dragFlags, swipeFlags)
+    }
+
+    override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+        return mItemMoveListener.onItemMove(viewHolder.adapterPosition, target.adapterPosition)
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        mItemMoveListener.onItemRemove(viewHolder.adapterPosition)
+    }
+
+    override fun isLongPressDragEnabled(): Boolean {
+        return true
+    }
+
+}
