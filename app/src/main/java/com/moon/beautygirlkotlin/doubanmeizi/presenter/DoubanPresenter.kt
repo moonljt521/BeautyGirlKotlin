ae8bde9fb2a7ffe95dfccaf80e4004d2ba7119f0
jiangtao.liang <jiangtao.liang@17zuoye.com>
2019-09-06 17:41:16 +0800

    fix code

--- app/src/main/java/com/moon/beautygirlkotlin/doubanmeizi/presenter/DoubanPresenter.kt
+++ app/src/main/java/com/moon/beautygirlkotlin/doubanmeizi/presenter/DoubanPresenter.kt
@@ -1,6 +1,5 @@
 package com.moon.beautygirlkotlin.doubanmeizi.presenter
 
-import com.moon.beautygirlkotlin.doubanmeizi.model.DoubanMeiziBody
 import com.moon.beautygirlkotlin.doubanmeizi.view.IDouBanView
 import com.moon.beautygirlkotlin.network.RetrofitHelper
 import com.moon.beautygirlkotlin.utils.DataUtil
@@ -19,41 +18,17 @@ import retrofit2.Response
 class DoubanPresenter : BaseMvpPresenter<IDouBanView>() {
 
     fun getDouBanMeiZiData(cid: Int, page: Int, type: Int) :Job{
-
-//       return executeRequest<List<DoubanMeiziBody>>(
-//                context,
-//                request = {
-//                    val response : Response<ResponseBody> = RetrofitHelper.getDoubanMeiziApi().getDoubanMeizi(cid, page)
-//                    DataUtil.getDouBanList(type,response)
-//                },
-//
-//                onSuccess = {
-//                    if (it.size <= 0) {
-//                        mvpView?.showError()
-//                    }else{
-//                        mvpView?.showSuccess(it)
-//                    }
-//                },
-//
-//                onFail = {
-//                    mvpView?.showError()
-//                }
-//        )
        return presenterScope.launch {
             try {
-                val data = getData(cid,page,type)
+                val data = withContext(Dispatchers.IO){
+                    val result: Response<ResponseBody> = RetrofitHelper.getDoubanMeiziApi().getDoubanMeizi(cid, page)
+                    DataUtil.getDouBanList(type,result)
+                }
                 mvpView?.showSuccess(data)
             }catch (e : Exception){
                 e.printStackTrace()
                 mvpView?.showError()
             }
-        }
-    }
-
-    suspend fun getData(cid: Int, page: Int , type: Int): List<DoubanMeiziBody> {
-        return withContext(Dispatchers.IO){
-            val result: Response<ResponseBody> = RetrofitHelper.getDoubanMeiziApi().getDoubanMeizi(cid, page)
-            DataUtil.getDouBanList(type,result)
         }
     }
 }
--- mvp-module/src/main/java/com/moon/mvpframework/factory/CreatePresenter.kt
+++ mvp-module/src/main/java/com/moon/mvpframework/factory/CreatePresenter.kt
@@ -5,11 +5,6 @@ import com.moon.mvpframework.presenter.BaseMvpPresenter
 import java.lang.annotation.Inherited
 import kotlin.reflect.KClass
 
-/**
- * @author 刘镓旗
- * @date 2017/11/17
- * @description 标注创建Presenter的注解
- */
 @Inherited
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class CreatePresenter(val value: KClass<out BaseMvpPresenter<*>>)
