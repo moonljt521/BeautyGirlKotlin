b9d5d79de5ddbe13cd79868bb9e16a0af936e562
jiangtao.liang <jiangtao.liang@17zuoye.com>
2019-09-09 15:47:53 +0800

    fix bug:没有目标分享地址

--- app/src/main/java/com/moon/beautygirlkotlin/utils/ShareUtil.kt
+++ app/src/main/java/com/moon/beautygirlkotlin/utils/ShareUtil.kt
@@ -12,11 +12,13 @@ class ShareUtil {
 
     companion object {
         fun shareAppLink(context: Context ,url: String, title: String ){
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_SUBJECT, "分享")
-            intent.putExtra(Intent.EXTRA_TEXT, "google play:" + url)
-            context.startActivity(Intent.createChooser(intent, title))
+            try {
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "text/plain"
+                intent.putExtra(Intent.EXTRA_SUBJECT, "分享")
+                intent.putExtra(Intent.EXTRA_TEXT, "google play:" + url)
+                context.startActivity(Intent.createChooser(intent, title))
+            }catch (e : Exception){}
         }
     }
 }
